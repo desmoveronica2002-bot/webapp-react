cf1340ca62504dc53fd67aeca84dc10f795b4f62
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-04-21 18:15:39 +0200

    create nav bar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,39 @@
+import { Link } from "react-router-dom";
+
 export default function AppHeader() {
-    return (
-        <header>
-            <h1>Header</h1>
-        </header>
-    )
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+
+          <a className="navbar-brand" href="a">Films Reviews</a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+
+              <li className="nav-item">
+                <a className="nav-link active" href="a">Home</a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="a">Films</a>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,4 +1,7 @@
 import { Outlet } from 'react-router-dom'
+import AppHeader from '../components/AppHeader'
+import AppFooter from '../components/AppFooter'
+
 
 export default function DefaultLayout() {
   return (
