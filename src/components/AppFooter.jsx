178a5e7a908582eb5eccfbbfbbd681934c21b82f
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-04-21 18:24:05 +0200

    continue footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,10 @@
 export default function AppFooter() {
     return (
-        <footer>
-            <p>footer</p>
+        <footer className="bg-light text-center text-lg-start">
+            <div className="text-center p-3">
+                @2024 Copyright:
+                <a className="text-dark" href="https://example.com"></a>
+            </div>
         </footer>
     )
 }
