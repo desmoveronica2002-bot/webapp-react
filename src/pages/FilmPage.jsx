33e8e7c39c8ba75edd5b53fcde87812e37a8bc7d
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-04-22 13:20:04 +0200

    create option review

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -21,7 +21,7 @@ export default function FilmPage() {
 
   return (
     <>
-     <div className="container-fluid">
+     <div className="container-fluid bg-light">
       <div className="row align-items-md-stretch">
         <div className="col-md-6">
           <div className="h-100 p-5 text-white">
@@ -43,8 +43,39 @@ export default function FilmPage() {
           </div>
 
           </div>
-        </div>
+     </div>
 
+     <div  className="container mt-5">
+      <h3>Submit and Reviews</h3>
+        <form>
+
+          <div className="mb-3">
+            <label htmlFor="reviewerName" className="form-label">Your Name</label>
+            <input type="text" className="form-control" id="reviewerName" placeholder="Enter your name"/>
+          </div>
+
+
+          <div className="mb-3">
+            <label htmlFor="review" className="form-label">Your Vote</label>
+            <select className="form-select" id="reviewVote">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5 </option>
+            </select>
+          </div>
+
+
+          <div className="mb-3">
+            <label htmlFor="reviewText" className="form-label">Your Review</label>
+            <textarea className="form-control" id="reviewText" rows="3" placeholder="Write your review here..."></textarea>
+          </div>
+
+          <button type="submit" className="btn btn-primary">Submit Review</button>
+        </form>
+    </div>
+     
   
 
     </>
